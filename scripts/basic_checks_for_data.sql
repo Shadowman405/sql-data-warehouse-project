45cf6bebf1d49c403082366638ae41f1243b0398
Maxim <61054270+Shadowman405@users.noreply.github.com>
2025-10-06 21:49:20 +0300

    Update basic_checks_for_data.sql
    new checks for data

--- scripts/basic_checks_for_data.sql
+++ scripts/basic_checks_for_data.sql
@@ -12,3 +12,13 @@ WHERE cst_lastname != TRIM(cst_lastname)
 --- Data Standartization & Consistency
 SELECT DISTINCT cst_marital_status
 FROM silver.crm_cust_info
+
+--- Check for nulls or negative numbers
+SELECT prd_cost
+FROM DataWareHouse.bronze.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL
+
+--- Check invalid dates 
+SELECT *
+FROM DataWareHouse.bronze.crm_prd_info
+WHERE prd_end_dt < prd_start_dt
