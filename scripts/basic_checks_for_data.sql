459c1f2fb8862e3a316147ef5fe7e039d24da2b6
Maxim <61054270+Shadowman405@users.noreply.github.com>
2025-10-09 21:09:54 +0300

    Update basic_checks_for_data.sql

--- scripts/basic_checks_for_data.sql
+++ scripts/basic_checks_for_data.sql
@@ -39,3 +39,21 @@ SELECT *
 FROM DataWareHouse.bronze.crm_sales_details
 WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
 
+
+--- Check no NULLS or Negative Numbers or Zero, Sales = Qnt * Price
+SELECT DISTINCT
+sls_sales AS old_sls_sales,
+sls_quantity,
+sls_price AS old_sls_price,
+CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
+	THEN sls_quantity * ABS(sls_price)
+	ELSE sls_sales
+END AS sls_sales,
+CASE WHEN sls_price IS NULL OR sls_price <= 0
+	THEN sls_sales / NULLIF(sls_quantity, 0)
+	ELSE sls_price
+END AS sls_price
+FROM DataWareHouse.bronze.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <= 0 OR sls_quantity <=0 OR sls_price <= 0
