2b6285cd129140cf9153455ae522dde20f7d242f
Maxim <61054270+Shadowman405@users.noreply.github.com>
2025-10-12 19:31:57 +0300

    Update proc_load_silver.sql

--- scripts/sliver/proc_load_silver.sql
+++ scripts/sliver/proc_load_silver.sql
@@ -116,3 +116,16 @@ CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
 END AS gen
 FROM DataWareHouse.bronze.erp_cust_az12
 
+--- Insert erp_loc_a101
+INSERT INTO DataWareHouse.silver.erp_loc_a101(
+	cid,
+	cntry
+)
+SELECT 
+REPLACE(cid, '-', '') AS cid,
+CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+	WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+	WHEN TRIM (cntry) = '' OR cntry IS NULL THEN 'n/a'
+	ELSE cntry
+END AS cntry
+FROM DataWareHouse.bronze.erp_loc_a101
